704c881cb2c9a7fc41a42f7cebf753ebb56f26e5
badatgames101 <167321916+badatgames101@users.noreply.github.com>
2024-05-23 23:58:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+//do later//
+const aboutMe = document.querySelector('#aboutMeSeciton');
+const aboutme_section = document.querySelector('.aboutMeSeciton');
+
+VBscroll.addEventListener('click', () => {
+   if(document.getElementById('aboutme_section').style.opacity = 0.1 ) {
+    document.getElementById('aboutme_section').style.transform = "600px";
+   } else {
+    document.getElementById(aboutme_section).style.transform = "200px";
+   }
+});
+
